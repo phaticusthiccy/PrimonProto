1038775b6693a598191a3458c347c6b45bedb7b9
Thiccy <birhic023@gmail.com>
2022-04-14 00:43:13 +0300

    Update start.js

--- start.js
+++ start.js
@@ -1,8 +1,9 @@
 const sulla = require('sulla');
 sulla.create().then((client) => start(client));
 
-const start = (client) => {
-    client.onMessage(async (message) => {
-        console.log(message)
-    })
-})
+function start(client) {
+  client.onMessage(async (message) => {
+      console.log(message)
+  })
+}
+
