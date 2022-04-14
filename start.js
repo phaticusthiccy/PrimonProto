604901c89791a004f69a400a484eecbde133a63b
Thiccy <birhic023@gmail.com>
2022-04-14 15:38:21 +0300

    Update start.js

--- start.js
+++ start.js
@@ -29,26 +29,29 @@ const { tmpdir } = require("os")
 const Crypto = require("crypto")
 const ff = require('fluent-ffmpeg')
 const webp = require("node-webpmux")
+const { state, saveState } = useSingleFileAuthState('./proto.json')
 
 // fs.writeFileSync("db.json", JSON.parse(JSON.stringify(process.env.SESSION)));
 
-
-const PrimonProto = makeWASocket({ printQRInTerminal: true }) 
-
-const connect = async () => {
-   
-     PrimonProto.ev.on('connection.update', async (update) => {
-        const { connection, lastDisconnect } = update
-        if(connection === 'close') {
-            connect()
-        } else {
-            await PrimonProto.sendMessage(PrimonProto.user.id, { text: "Merhaba" })
-        }
+async function connectPrimon()  {
+    const PrimonProto = makeWASocket({
+            logger: pino({ level: 'silent' }),
+            printQRInTerminal: true,
+            auth: state,
+            browser: ['Primon Proto', 'Chrome', '1.0'],
     })
-    
-    PrimonProto.ev.on("messages.upsert", async (msg) => {
-        console.log(msg)
+    PrimonProto.ev.on('messages.upsert', async m => {
+            if (!m.messages) return
+             console.log(m)
      })
+    PrimonProto.ev.on('connection.update', (update) => {
+            const { connection, lastDisconnect } = update
+            if (connection === 'close') {
+               
+                lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? connectPrimon() : console.log('Connection Logged Out..')
+            }
+     })
+    PrimonProto.ev.on('creds.update', () => saveState)
 }
 
-connect()
+connectPrimon()
