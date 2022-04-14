4e91ccc8191cf626d0faa4d23dc8277722eb8c98
Thiccy <birhic023@gmail.com>
2022-04-14 17:02:58 +0300

    Update start.js

--- start.js
+++ start.js
@@ -1,67 +1,67 @@
-const { 
-    default: makeWASocket, 
-    DisconnectReason, 
-    useSingleFileAuthState, 
-    fetchLatestBaileysVersion, 
+const {
+    default: makeWASocket,
+    DisconnectReason,
+    useSingleFileAuthState,
+    fetchLatestBaileysVersion,
     delay,
     BufferJSON,
     makeInMemoryStore,
-    jidNormalizedUser, 
-    makeWALegacySocket, 
+    jidNormalizedUser,
+    makeWALegacySocket,
     useSingleFileLegacyAuthState,
     DEFAULT_CONNECTION_CONFIG,
     DEFAULT_LEGACY_CONNECTION_CONFIG,
     extensionForMediaMessage,
-    extractMessageContent, 
-    getContentType, 
+    extractMessageContent,
+    getContentType,
     normalizeMessageContent,
-    proto, 
-    downloadContentFromMessage
-} = require("@adiwajshing/baileys")
-__path = process.cwd()
-var express = require('express');
-var secure = require('ssl-express-www');
-var cors = require('cors');
-var router  = express.Router();
-const fs = require("fs")
-const util = require("util")
-const pino = require("pino")
-const path = require("path")
-const { Boom } = require("@hapi/boom")
-const FileType = require("file-type")
-const moment = require("moment-timezone")
-const { tmpdir } = require("os")
-const Crypto = require("crypto")
-const ff = require('fluent-ffmpeg')
-const webp = require("node-webpmux")
-const { state, saveState } = useSingleFileAuthState('./proto.json')
+    proto,
+    downloadContentFromMessage,
+} = require("@adiwajshing/baileys");
+const { exec } = require("child_process");
 
-var PrimonProto;
-router.get("/", async (req, res, next) => {
-    PrimonProto =  makeWASocket({
-                logger: pino({ level: 'silent' }),
-                printQRInTerminal: true,
-                auth: state,
-                browser: ['Primon Proto', 'Chrome', '1.0'],
-            })
-     res.send(PrimonProto)
-})
-// fs.writeFileSync("db.json", JSON.parse(JSON.stringify(process.env.SESSION)));
+const fs = require("fs");
+const util = require("util");
+const pino = require("pino");
+const path = require("path");
+const { Boom } = require("@hapi/boom");
+var readline = require("readline");
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const { state, saveState } = useSingleFileAuthState("proto.json");
 
-async function connectPrimon()  {
-    
-    PrimonProto.ev.on('messages.upsert', async m => {
-            if (!m.messages) return
-             console.log(m)
-     })
-    PrimonProto.ev.on('connection.update', (update) => {
-            const { connection, lastDisconnect } = update
-            if (connection === 'close') {
-               
-                lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? connectPrimon() : console.log('Connection Logged Out..')
+async function connectPrimon() {
+    var PrimonProto = makeWASocket({
+        logger: pino({ level: "silent" }),
+        printQRInTerminal: true,
+        auth: state,
+        browser: ["Primon Proto", "Chrome", "1.0"],
+    });
+    PrimonProto.ev.on("connection.update", (update) => {
+        const { connection, lastDisconnect } = update;
+        if (connection === "close") {
+            lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
+                ? connectPrimon()
+                : console.log("Connection Logged Out..");
+        }
+        exec("npm i -g @railway/cli", (err, stdout, stderr) => {
+            if (err) {
+                console.error(err);
+            } else {
+              exec("clear", (err, stdout, stderr) => {
+                if (err) {
+                    exec("cls", (err, stdout, stderr) => {});
+                } else {
+                  exec("railway up", (err, stdout, stderr) => {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log(`stdout: ${stdout}`);
+                    }
+                  });
+                }
+              });
             }
-     })
-    PrimonProto.ev.on('creds.update', () => saveState)
+        });
+    });
 }
-
-connectPrimon()
+connectPrimon();
