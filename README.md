730581fc6e21236e4a00b74265218ae7d562f6b7
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2022-05-29 18:04:21 +0300

    Base 0

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
   <h1> PrimonProto </h1>
   
 Configurational ESM Lightweigth WebSocket Tool. Uses side clients to connect firewall bridge. 
+A.K.A Primon Proto Whatsapp Bot
   </center>
 </html>
--- save_db_store.js
+++ save_db_store.js
@@ -1,3 +1,12 @@
+// Primon Proto
+// Headless WebSocket, type-safe Whatsapp UserBot
+//
+// Primon, lisanced under GNU GENERAL PUBLIC LICENSE. May cause some warranity problems, within Priomon.
+// Multi-Device Lightweight ES5 Module (can ysable with mjs)
+//
+// Phaticusthiccy - 2022
+
+
 const { Octokit } = require("@octokit/core");
 const fs = require("fs");
 const octokit = new Octokit({
