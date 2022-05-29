006341f71e89011c4d7f1bf20dea68f5e160fa79
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2022-05-30 01:02:49 +0300

    Base 0

--- save.js
+++ save.js
@@ -12,7 +12,7 @@ require('util').inspect.defaultOptions.depth = null
 var SN = process.env.SESSION
 async function save(st) {
   var sn = ""
-  sn = Buffer.from(st, "base64").toString()
+  sn = Buffer.from(SN, "base64").toString()
   fs.writeFileSync("session.json", sn)
   console.log("Primon Session Updated!")
   return true
