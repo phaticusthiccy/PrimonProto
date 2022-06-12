b67961af7a9c709e69b3d8318c2d97b231b275e7
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2022-06-12 08:34:31 +0300

    Base 0

--- local/local.sh
+++ local/local.sh
@@ -12,19 +12,19 @@ echo "┌───────────────────────�
 |                                           |
 └───────────────────────────────────────────┘
 "
-pkg update
-pkg upgrade
-pkg install nodejs
-npm install @railway/cli
-npm install -g typescript
+yes n | pkg update
+yes n | pkg upgrade
+yes n | pkg install nodejs
+yes n | npm install @railway/cli
+yes n | npm install -g typescript
 npm install pino
-npm install @hapi/boom
-npm install @adiwajshing/baileys
-npm install @adiwajshing/keyed-db
+yes n | npm install @hapi/boom
+yes n | npm install @adiwajshing/baileys
+yes n | npm install @adiwajshing/keyed-db
 npm install chalk@4.1.2
 npm install qrcode-terminal
-npm install shelljs
-npm install axios
+yes n | npm install shelljs
+yes n | npm install axios
 git clone https://github.com/phaticusthiccy/PrimonProto
 cd PrimonProto && cd local
 tsc local.ts
