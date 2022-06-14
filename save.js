62f5ed70a4b2dc300d36dce63c85cf5d9eb14662
Thiccy <74794033+phaticusthiccy@users.noreply.github.com>
2022-06-15 02:59:20 +0300

    Base 0

--- save.js
+++ save.js
@@ -11,9 +11,6 @@ const fs = require("fs")
 require('util').inspect.defaultOptions.depth = null
 
 async function save2() {
-  try {
-    fs.unlinkSync("./session/test")
-  } catch {}
   var sn = ""
   var name = Buffer.from(process.env.SESSION5, "base64").toString()
   var sn3 = name.split("&&&&&&&")
@@ -28,11 +25,14 @@ async function save2() {
   var sn2 = sn.split("&&&&&&&")
   sn2.map((Element) => {
     if (Element !== "" || Element !== " ") {
-      fs.writeFileSync("./session/" + sn4[sayac], Element)
+      fs.writeFileSync("session/" + sn4[sayac], Element)
     }
     sayac = sayac + 1
   })
   console.log("Primon Session Updated!")
+  try {
+    fs.unlinkSync("./session/test.txt")
+  } catch {}
   return true
 }
 save2()
