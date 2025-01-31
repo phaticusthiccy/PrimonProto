9a03261ee77d2a4a9cfc93e10dba0f2f1f62513d
Ic3zy <safiye51dundar51@gmail.com>
2025-01-31 20:55:36 +0300

    add .insta command

--- modules/instagram.js
+++ modules/instagram.js
@@ -1,5 +1,5 @@
 const instaDownload = require("ic3zy-api").downloads;
-const fs = require("fs")
+const fs = require("fs");
 addCommand({pattern: "^insta ?(.*)", desc: "Allows you to download videos/images from Instagram.", access: "all"}, async (msg, match, sock) => {
     const data = await instaDownload(match[1], __dirname+"/insta");
     if (data.type == "video") await sock.sendMessage(msg.key.remoteJid, {video: { url: data.path }, caption: data.info});
