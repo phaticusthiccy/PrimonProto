be237f95c245e255ac26d42bee5eb752375a3f84
phaticusthiccy <birhic023@gmail.com>
2025-02-02 17:18:06 +0300

    test update

--- modules/ping.js
+++ modules/ping.js
@@ -1,17 +1,14 @@
 addCommand({pattern: "^ping$", access: "all", desc: "_Meter the response time of the bot._"}, async (msg, match, sock, rawMessage) => {
     const startTime = Date.now();
+    let publicMessage;
     if (msg.key.fromMe) {
-        await sock.sendMessage(msg.key.remoteJid, { text: "Pong", edit: msg.key });
+        publicMessage = await sock.sendMessage(msg.key.remoteJid, { text: "Pong", edit: msg.key });
     } else {
-        var publicMessage = await sock.sendMessage(msg.key.remoteJid, { text: "Pong" }, { quoted: rawMessage.messages[0] });
+        publicMessage = await sock.sendMessage(msg.key.remoteJid, { text: "Pong" }, { quoted: rawMessage.messages[0] });
     }
-    const endTime = Date.now();
-    const responseTime = endTime - startTime;
-    if (msg.key.fromMe) {
-        const emoji = responseTime < 200 ? "⚡" : (responseTime < 400 ? "⏳" : "⚠️");
-        await sock.sendMessage(msg.key.remoteJid, { text: `_${emoji} Pong!_\n\n_Response time: ${responseTime}ms_`, edit: msg.key });
-    } else {
-        await sock.sendMessage(msg.key.remoteJid, { text: `_${emoji} Pong!_\n\n_Response time: ${responseTime}ms_`, edit: publicMessage.key });
-    }
+    const responseTime = Date.now() - startTime;
+    const emoji = responseTime < 200 ? "⚡" : (responseTime < 400 ? "⏳" : "⚠️");
+    const editKey = msg.key.fromMe ? msg.key : publicMessage.key;
+    await sock.sendMessage(msg.key.remoteJid, { text: `_${emoji} Pong!_\n\n_Response time: ${responseTime}ms_`, edit: editKey });
     return;
 })
